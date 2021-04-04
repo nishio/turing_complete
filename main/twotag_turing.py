b4cf72b2e39339dbe64f76ec9d1e379f10aa392a
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2021-04-04 17:07:43 +0900

    reorder

--- main/twotag_turing.py
+++ main/twotag_turing.py
@@ -44,6 +44,39 @@ def convert(turing_rules):
     return tag_rules
 
 
+def get_initial_queue(initial_state, left_tape, right_tape):
+    M = 0
+    for x in left_tape:
+        M *= 2
+        M += x
+    N = 0
+    for x in reversed(right_tape):
+        N *= 2
+        N += x
+    
+    ax = "ax"
+    bx = "bx"
+    initial_queue = [
+        f"{c}_{initial_state}"
+        for c in f"Ax{ax * M}Bx{bx * N}"
+    ]
+    return initial_queue
+
+
+def run_tag_system(turing_rules, initial_state, left_tape, right_tape):
+    tag_rules = convert(turing_rules)
+    queue = get_initial_queue(initial_state, left_tape, right_tape)
+    while len(queue) >= 2:
+        print(queue)
+        head = queue[0]
+        if head not in tag_rules:
+            break
+        queue = queue[2:] + tag_rules[head]
+    print(queue)
+    return queue
+
+
+
 turing_rules = {  # state: (write, move, if 0 goto, if 1 goto)
     "Qstart": (0, "R", "Q1_1", "Q1_0")
 }
@@ -74,33 +107,5 @@ def turing_step(rules, state, left_tape, right_tape):
     return new_state, left_tape, right_tape
 
 
-def run_tag_system(turing_rules, initial_state, left_tape, right_tape):
-    tag_rules = convert(turing_rules)
-    queue = get_initial_queue(initial_state, left_tape, right_tape)
-    while len(queue) >= 2:
-        print(queue)
-        head = queue[0]
-        if head not in tag_rules:
-            break
-        queue = queue[2:] + tag_rules[head]
-    print(queue)
-    return queue
 
 
-def get_initial_queue(initial_state, left_tape, right_tape):
-    M = 0
-    for x in left_tape:
-        M *= 2
-        M += x
-    N = 0
-    for x in reversed(right_tape):
-        N *= 2
-        N += x
-    
-    ax = "ax"
-    bx = "bx"
-    initial_queue = [
-        f"{c}_{initial_state}"
-        for c in f"Ax{ax * M}Bx{bx * N}"
-    ]
-    return initial_queue
